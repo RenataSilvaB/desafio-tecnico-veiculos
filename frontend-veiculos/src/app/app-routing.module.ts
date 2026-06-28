4fac1a71221fea6dca7701d32be80a430cda522e
RenataSilvaB <bsilvaren@gmail.com>
2026-06-28 12:21:21 -0300

    feat: configura rota de veiculos

--- frontend-veiculos/src/app/app-routing.module.ts
+++ frontend-veiculos/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VeiculosComponent } from './components/veiculos/veiculos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'veiculos', pathMatch: 'full' },
+  { path: 'veiculos', component: VeiculosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+

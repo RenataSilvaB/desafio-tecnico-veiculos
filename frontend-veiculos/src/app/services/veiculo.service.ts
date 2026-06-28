29beee4b846099ee1e44eee688d9473bdfd5c64a
RenataSilvaB <bsilvaren@gmail.com>
2026-06-28 18:39:42 -0300

    refactor: atualiza servico de veiculos

--- frontend-veiculos/src/app/services/veiculo.service.ts
+++ frontend-veiculos/src/app/services/veiculo.service.ts
@@ -1,29 +1,46 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Veiculo } from '../models/veiculo.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VeiculoService {
 
-  private apiUrl = 'http://localhost:3000/veiculos';
+  private apiUrl = `${environment.apiUrl}/veiculos`;
 
   constructor(private http: HttpClient) { }
 
-  listar(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+  listar(): Observable<Veiculo[]> {
+    return this.http.get<Veiculo[]>(this.apiUrl).pipe(
+      catchError(err => throwError(() => err))
+    );
   }
 
-  cadastrar(veiculo: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, veiculo);
+  buscarPorId(id: number): Observable<Veiculo> {
+    return this.http.get<Veiculo>(`${this.apiUrl}/${id}`).pipe(
+      catchError(err => throwError(() => err))
+    );
   }
 
-  editar(id: number, veiculo: any): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}/${id}`, veiculo);
+  cadastrar(veiculo: Omit<Veiculo, 'id'>): Observable<Veiculo> {
+    return this.http.post<Veiculo>(this.apiUrl, veiculo).pipe(
+      catchError(err => throwError(() => err))
+    );
   }
 
-  excluir(id: number): Observable<any> {
-    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+  editar(id: number, veiculo: Omit<Veiculo, 'id'>): Observable<Veiculo> {
+    return this.http.put<Veiculo>(`${this.apiUrl}/${id}`, veiculo).pipe(
+      catchError(err => throwError(() => err))
+    );
+  }
+
+  excluir(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`).pipe(
+      catchError(err => throwError(() => err))
+    );
   }
 }
